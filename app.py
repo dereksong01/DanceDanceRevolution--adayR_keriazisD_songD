7e2e2aec5272a3abc34383e0b0ed17c4905f503d
Ryan Aday <ryanobelisk@gmail.com>
2019-05-06 14:05:09 -0400

    added example map

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+
+
+app = Flask(__name__)
+app.secret_key = urandom(32)
+
+@app.route("/", methods=["GET"])
+def index():
+    '''Index takes current user and their posts '''
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
