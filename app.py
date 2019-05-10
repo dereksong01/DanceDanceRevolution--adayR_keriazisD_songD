5efc85386413facb592f1212afe25c3aa4a17dce
DK <dkeriazis@gmail.com>
2019-05-10 14:06:13 -0400

    Begin API for backend

--- app.py
+++ app.py
@@ -7,6 +7,54 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/create/<room_id>')
+def create(room_id):
+    pass
+
+@app.route('/join/<room_id>')
+def join(room_id):
+    pass
+
+@app.route('/room/<room_id>')
+def room(room_id):
+    pass
+
+@app.route('/start')
+def start():
+    """
+    Initializes and starts the game for a given room
+
+    Expects a POST in the following JSON format:
+    {
+        "room_id": <room_id>,
+        "players": [
+            "player_id": <player_id>,
+            "color": <color>,
+            "name": <name>
+         ]
+    }
+    """
+    pass
+
+@app.route('/turn')
+def turn():
+    """
+    Ends a given player's turn for a given room
+
+    Expects a POST in the following JSON format:
+    {
+        "room_id": <room_id>,
+        "player_id": <player_id>,
+        "line": [
+            {
+                "x": <x>,
+                "y": <y>
+            }
+        ]
+    }
+    """
+    pass
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
